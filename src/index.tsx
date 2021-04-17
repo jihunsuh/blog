011ccd87b16bd8c0abe1eefcea03ec59ad30d352
jihunsuh <jihunsuh12@gmail.com>
2021-04-17 15:18:20 +0900

    initial reusable-react app setting

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
+import { ReusableProvider } from '@entropyparadox/reusable-react';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ReusableProvider>
+      <App />
+    </ReusableProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
